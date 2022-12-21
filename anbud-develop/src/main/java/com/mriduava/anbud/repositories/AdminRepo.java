caa5fc4b418e8dc33c53a47ca419e785ffc4270a
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2022-12-21 16:59:12 +0700

    push code

---
+++ anbud-develop/src/main/java/com/mriduava/anbud/repositories/AdminRepo.java
@@ -0,0 +1,11 @@
+package com.mriduava.anbud.repositories;
+
+import com.mriduava.anbud.entities.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepo extends JpaRepository<Admin, Long> {
+    Admin findByEmail(String email);
+    
+}
